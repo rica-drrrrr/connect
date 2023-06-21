7a51b595b84c50afcc7de56fcf7d3068db830abb
rica-drrrrr <omuraisu0141@ezweb.ne.jp>
2023-06-22 02:23:01 +0900

    init commit

---
+++ production.js
@@ -0,0 +1,27 @@
+function toggleNav() {
+  var body = document.body;
+  var hamburger = document.getElementById('js-hamburger');
+  var blackBg = document.getElementById('js-black-bg');
+
+  hamburger.addEventListener('click', function() {
+    body.classList.toggle('nav-open');
+  });
+  blackBg.addEventListener('click', function() {
+    body.classList.remove('nav-open');
+  });
+}
+toggleNav();
+
+
+
+window.addEventListener("scroll", function () {
+  const elm = document.querySelector(".elm");
+  const scroll = window.pageYOffset;
+  if (scroll > 300) {
+    elm.style.opacity = "1";
+    // console.log(scroll);
+  } else {
+    elm.style.opacity = "0";
+    // console.log(scroll);
+  }
+});
